bc1e978db1b1d7a1a8655aa4ac363b65dd77a4b7
NgoXuanThang <thang.nx@novaeforce.vn>
2022-08-25 13:16:17 +0700

    create footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.scss';
 import Header from './components/organisms/Header';
+import Footer from './components/organisms/Footer';
+
+
 
 function App() {
   return (
     <>
       <Header/>
+      <Footer/>
     </>
   );
 }
--- src/components/atoms/Image/index.js
+++ src/components/atoms/Image/index.js
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import mapModifiers from '../../../utils/functions';
 
-const Ratio = ['1x1', '4x3', '16x9', '2x1', 'logo'];
+const Ratio = ['1x1', '4x3', '16x9', '2x1', 'logo', '128x48'];
 const SizeImageType = ['cover' | 'contain' | 'inherit'];
 
 export default function Image({ratio, imgSrc, alt, imgType}) {
