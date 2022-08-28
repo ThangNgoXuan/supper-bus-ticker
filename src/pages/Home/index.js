3bc209c245ca3deedbad2c081b87da26655cb678
NgoXuanThang <thang.nx@novaeforce.vn>
2022-08-28 19:31:26 +0700

    create component

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import HeroBanner from '../../components/organisms/HeroBanner'
+import PopularRoute from '../../components/organisms/PopularRoute'
 import TopDestination from '../../components/organisms/TopDestinations'
 
 export default function Home() {
   return (
     <div className='p-home'>
         <HeroBanner />
+        <PopularRoute />
         <TopDestination/>
-
     </div>
   )
 }
