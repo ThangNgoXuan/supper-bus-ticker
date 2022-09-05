e66b9667af31de3893a934e68d19865c0fa719ba
NgoXuanThang <ngoxuanthangnyc@gmail.com>
2022-09-05 17:42:41 +0700

    fix

--- src/pages/Schedule/index.js
+++ src/pages/Schedule/index.js
@@ -2,17 +2,46 @@ import React from "react";
 import HeroBanner from "../../components/organisms/HeroBanner";
 import Input from "../../components/atoms/Input";
 import Text from "../../components/atoms/Text";
-import Button from '../../components/atoms/Button';
+import Button from "../../components/atoms/Button";
 import Link from "../../components/atoms/Link";
+import { FiSearch } from "react-icons/fi";
 
 const title = "Tìm chuyến xe";
 const dataList = new Array(20).fill({
   addressFrom: "Hồ Chí Minh",
   addressEnd: [
-    { address: "Hà Nội", time: "8", km: "300", type: "Giường", linkDetail: '/chi-tiet-chuyen-xe', linkBook: "/dat-ve" },
-    { address: "Hà Nội", time: "8", km: "300", type: "Giường", linkDetail: '/chi-tiet-chuyen-xe', linkBook: "/dat-ve" },
-    { address: "Hà Nội", time: "8", km: "300", type: "Giường", linkDetail: '/chi-tiet-chuyen-xe', linkBook: "/dat-ve" },
-    { address: "Hà Nội", time: "8", km: "300", type: "Giường", linkDetail: '/chi-tiet-chuyen-xe', linkBook: "/dat-ve" },
+    {
+      address: "Hà Nội",
+      time: "8",
+      km: "300",
+      type: "Giường",
+      linkDetail: "/chi-tiet-chuyen-xe",
+      linkBook: "/dat-ve",
+    },
+    {
+      address: "Hà Nội",
+      time: "8",
+      km: "300",
+      type: "Giường",
+      linkDetail: "/chi-tiet-chuyen-xe",
+      linkBook: "/dat-ve",
+    },
+    {
+      address: "Hà Nội",
+      time: "8",
+      km: "300",
+      type: "Giường",
+      linkDetail: "/chi-tiet-chuyen-xe",
+      linkBook: "/dat-ve",
+    },
+    {
+      address: "Hà Nội",
+      time: "8",
+      km: "300",
+      type: "Giường",
+      linkDetail: "/chi-tiet-chuyen-xe",
+      linkBook: "/dat-ve",
+    },
   ],
 });
 
@@ -31,11 +60,16 @@ export default function Schedule() {
       <HeroBanner />
       <div className="p-schedule">
         <div className="p-schedule_searchWrap">
-          <Text>Search chuyến đi</Text>
-          <Input />
+          <Text modifiers={["22x32", "coolBlack", '600']}>Search chuyến đi</Text>
+          <div className="p-schedule_searchWrap_search">
+            <Input placeholder="Nhập điểm đến" />
+            <Button>
+              <FiSearch />
+            </Button>
+          </div>
         </div>
         <div className="p-schedule_title">
-          <Text>{title}</Text>
+          <Text modifiers={["22x32", "coolBlack", '600']}>{title}</Text>
         </div>
         <div className="p-schedule_listSchedule">
           <table className="p-schedule_table">
@@ -43,7 +77,10 @@ export default function Schedule() {
               {listTh &&
                 listTh.length > 0 &&
                 listTh.map((item, index) => (
-                  <th key={`item-th-${index.toString()}`} className="p-schedule_table_th">
+                  <th
+                    key={`item-th-${index.toString()}`}
+                    className="p-schedule_table_th"
+                  >
                     <Text>{item.item}</Text>
                   </th>
                 ))}
@@ -58,17 +95,38 @@ export default function Schedule() {
                   className="p-schedule_table_item"
                 >
                   <thead className="p-schedule_table_thead">
-                    <Text modifiers={['22x32', '600', 'electricCrimson']}>{item.addressFrom}</Text>
+                    <Text modifiers={["22x32", "600", "electricCrimson"]}>
+                      {item.addressFrom}
+                    </Text>
                   </thead>
                   <tbody className="p-schedule_table_tbody">
                     {item.addressEnd.map((ele, idx) => (
-                      <tr key={`item-placeEnd-${idx.toString()}`} className='p-schedule_table_tbody_tr'>
-                        <th className="p-schedule_table_tbody_th">{ele.address}</th>
-                        <th className="p-schedule_table_tbody_th">{ele.type}</th>
+                      <tr
+                        key={`item-placeEnd-${idx.toString()}`}
+                        className="p-schedule_table_tbody_tr"
+                      >
+                        <th className="p-schedule_table_tbody_th">
+                          {ele.address}
+                        </th>
+                        <th className="p-schedule_table_tbody_th">
+                          {ele.type}
+                        </th>
                         <th className="p-schedule_table_tbody_th">{ele.km}</th>
-                        <th className="p-schedule_table_tbody_th">{ele.time}</th>
-                        <th className="p-schedule_table_tbody_th"><Link href={ele.linkDetail}><Text modifiers={['coolBlack']}>Chi tiết</Text></Link></th>
-                        <th className="p-schedule_table_tbody_th"><Link href={ele.linkBook}><Button><Text modifiers={['white']}>Đặt vé</Text></Button></Link></th>
+                        <th className="p-schedule_table_tbody_th">
+                          {ele.time}
+                        </th>
+                        <th className="p-schedule_table_tbody_th">
+                          <Link href={ele.linkDetail}>
+                            <Text modifiers={["coolBlack"]}>Chi tiết</Text>
+                          </Link>
+                        </th>
+                        <th className="p-schedule_table_tbody_th">
+                          <Link href={ele.linkBook}>
+                            <Button>
+                              <Text modifiers={["white"]}>Đặt vé</Text>
+                            </Button>
+                          </Link>
+                        </th>
                       </tr>
                     ))}
                   </tbody>
